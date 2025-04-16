4447b227154eaf9c4e6aa819941bec646e7cc1bd
mgzs2013 <mariog.2822@gmail.com>
2025-04-16 16:24:56 -0700

    Removed SVG in hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,16 +7,11 @@ const Hero: React.FC = () => {
   return (
     <section className={styles.hero}>
       <div className={styles.welcome}>
-        <h1>Hello and welcome!</h1>
-        <h2>It&apos;s me, Mario.</h2>
-        <div className={styles.toolset}>
-          <Image 
-            src="/tool-kit.png" // Path to your tool set PNG
-            alt="Tool Set" 
-            width={70} // Set the width of the tool set image
-            height={35} // Set the height of the tool set image
-          />
-        </div>
+        <h1>Hello</h1>
+        <h1>and welcome!</h1>
+        <h2>It&apos;s me,</h2>
+        <h2>Mario.</h2>
+
       </div>
 
     </section>
