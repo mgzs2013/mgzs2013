e99eade134da12cadd35a53c3c94d8182485b5ef
mgzs2013 <mariog.2822@gmail.com>
2025-04-16 11:42:42 -0700

    updates

--- components/ContentRectangle.tsx
+++ components/ContentRectangle.tsx
@@ -1,5 +1,6 @@
 // components/ContentRectangle.tsx
 import styles from '../styles/ContentRectangle.module.css';
+import Image from 'next/image';
 
 
 interface ContentRectangleProps {
@@ -18,7 +19,22 @@ const ContentRectangle: React.FC<ContentRectangleProps> = ({content, contentType
           <div className={styles.smallRectangle}>Rectangle 3</div>
           <div className={styles.smallRectangle}>Rectangle 4</div>
         </div><div className={styles.footerText}>
-            This is some footer text.
+            Check them all out: 
+            <a 
+              href="https://github.com/mgzs2013" // Replace with your actual GitHub profile link
+              target="_blank" 
+              rel="noopener noreferrer" 
+              className={styles.githubLink} // Optional: add a class for styling
+            >
+              <Image 
+                src="/GitText.svg" // Path to your GitHub SVG
+                alt="GitHub" 
+                width={45} // Adjust width as needed
+                height={20} // Adjust height as needed
+                className={styles.githubIcon} // Optional: add a class for styling
+              />
+            </a>
+
           </div></>
     )}
     {contentType === 'list' && (
